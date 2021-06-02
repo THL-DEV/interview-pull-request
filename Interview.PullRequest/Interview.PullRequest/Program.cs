4ccccf6ff5434c318af45c9329b4f891e1ef2a52
Julian Adler <julian.a@passport.com>
2021-06-02 18:01:57 +1200

    Scaffold project

---
+++ Interview.PullRequest/Interview.PullRequest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interview.PullRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
